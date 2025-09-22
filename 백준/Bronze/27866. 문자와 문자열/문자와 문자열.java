cbe7f315c13d5cca122297e837258c5b8224fb27
Bucheo <49815759+Bucheo@users.noreply.github.com>
2025-09-23 00:27:30 +0900

    [Bronze V] Title: 문자와 문자열, Time: 168 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt() - 1;
+
+        System.out.println(str.charAt(n));
+    }
+}
