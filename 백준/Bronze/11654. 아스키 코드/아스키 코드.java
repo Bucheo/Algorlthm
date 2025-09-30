715730accf1326be7cd43b32bd5e2fb02f0279ec
Bucheo <49815759+Bucheo@users.noreply.github.com>
2025-09-30 20:55:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        int b = a;
+        System.out.println(b);
+    }
+}
