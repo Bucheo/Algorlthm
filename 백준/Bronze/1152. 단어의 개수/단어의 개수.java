169de036ca46d5007a7583d69dd3ec31c79f9cdd
Bucheo <49815759+Bucheo@users.noreply.github.com>
2025-10-01 22:31:44 +0900

    [Bronze II] Title: 단어의 개수, Time: 456 ms, Memory: 30800 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        System.out.println(st.countTokens());
+    }
+}
